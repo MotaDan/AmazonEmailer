349d9aafef3c297f84d9719dd6e2cb64020fed1b
Daniel Mota <MotaDan@gmail.com>
2017-06-11 23:49:09 -0400

    Adding check for failure because amazon has flagged the program as a bot. Also saving the failed page for future viewing.

--- amazonemailer/amazonemailer.py
+++ amazonemailer/amazonemailer.py
@@ -187,9 +187,14 @@ class AmazonEmailer:
                 try:
                     next_page = "https://www.amazon.com" + asoup.find('a', id="pagnNextLink")['href']
                 except TypeError:
-                    print("Error: No more pages, range higher than number of items.")
+                    if asoup.head.title.string != "Robot Check":
+                        print("Error: No more pages, range higher than number of items.")
+                    else:
+                        print("You've been discovered as a bot. Take a break and come back tomorrow.")
                     print(categorystr)
-                    print(asoup)
+                    with open("./output/failed_{}.html".format(categorystr.replace(" ", "")), 'w') as f:
+                        f.write(asoup.prettify())
+                        print("Failed html written to ./output/failed_{}.html".format(categorystr.replace(" ", "")))
                     break
 
                 time.sleep(1)
